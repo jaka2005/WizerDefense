ae9efae07e25af4d27a74b13ab86811c0420f0ec
j2k <jakalogin1808@gmail.com>
2022-04-28 20:03:10 +0400

    added main menu scene

--- src/commonMain/kotlin/MainMenuScene.kt
+++ src/commonMain/kotlin/MainMenuScene.kt
@@ -1,13 +1,105 @@
+import com.soywiz.korge.input.mouse
+import com.soywiz.korge.input.onClick
 import com.soywiz.korge.scene.Scene
-import com.soywiz.korge.view.Container
-import com.soywiz.korge.view.position
-import com.soywiz.korge.view.roundRect
+import com.soywiz.korge.tween.get
+import com.soywiz.korge.tween.tween
+import com.soywiz.korge.view.*
 import com.soywiz.korim.color.Colors
+import com.soywiz.korim.format.readBitmap
+import com.soywiz.korio.file.std.resourcesVfs
+import com.soywiz.korma.geom.Point
+import com.soywiz.korma.interpolation.Easing
+import kotlin.properties.Delegates
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainMenuScene : Scene() {
+    private var backgroundLayer: Container by Delegates.notNull()
+    private var uiLayer: Container by Delegates.notNull()
+
+    private var wizerRect: RoundRect by Delegates.notNull()
+    private var bulletRect: RoundRect by Delegates.notNull()
+
+    private var wizer1: Image by Delegates.notNull()
+    private var bullet1: Image by Delegates.notNull()
+    private var wizer2: Image by Delegates.notNull()
+    private var bullet2: Image by Delegates.notNull()
+
+    private var cursorBG: Image by Delegates.notNull()
+
     override suspend fun Container.sceneInit() {
-        val startButton = roundRect(200, 50, 7, fill = Colors.LIGHTGREY) {
-            position((screenWidth - width) / 2 , (screenHeight - height) / 2)
+        backgroundLayer = container()
+        uiLayer = container()
+
+        val wizerImage1 = resourcesVfs["wizer_pngs/wizer_explains.png"].readBitmap()
+        val bulletImage1 = resourcesVfs["wizer_pngs/bullet_fucks.png"].readBitmap().flipX()
+        val wizerImage2 = resourcesVfs["wizer_pngs/wizer_puzzled.png"].readBitmap()
+        val bulletImage2 = resourcesVfs["wizer_pngs/bullet_yippee2.png"].readBitmap().flipX()
+
+        val cursorImage = resourcesVfs["cursor.png"].readBitmap()
+
+        wizer2 = backgroundLayer.image(wizerImage2) {
+            scale = 175 / width
+            position(-15, 120)
+        }
+        wizer1 = backgroundLayer.image(wizerImage1) {
+            scale = 230 / width
+            position(0, 95)
+        }
+        wizerRect = roundRect(105.0, wizer1.height * wizer1.scaleY - 50, .0) {
+            alpha = .0
+            x = 30.0
+            y = wizer1.y + 50
+        }
+
+        bullet1 = backgroundLayer.image(bulletImage1) {
+            scale = 200 / width
+            position(300, 100)
+        }
+        bullet2 = backgroundLayer.image(bulletImage2) {
+            visible = false
+            scale = 200 / width
+            position(350, 110)
+        }
+        bulletRect = roundRect(100.0, bullet1.height * bullet1.scaleY - 45, .0) {
+            alpha = .0
+            x = bullet1.x + 85.0
+            y = bullet1.y + 45
+        }
+
+        cursorBG = backgroundLayer.image(cursorImage) {
+            position(randomPoint())
+        }
+
+        val startButton = uiLayer.roundRect(200, 50, 7, fill = Colors.DARKGREY) {
+            alpha = .5
+            position((screenWidth - width) / 2 , (screenHeight - height) / 2 - 100)
+            onClick { println("start") }
+        }
+        uiLayer.text("Начать игру").centerOn(startButton)
+    }
+
+    override suspend fun Container.sceneMain() {
+        wizerRect.addUpdater {
+            wizer1.visible = !mouse.isOver and !collidesWith(cursorBG)
+            wizer2.visible = mouse.isOver or collidesWith(cursorBG)
+        }
+        bulletRect.addUpdater {
+            bullet1.visible = !mouse.isOver and !collidesWith(cursorBG)
+            bullet2.visible = mouse.isOver or collidesWith(cursorBG)
+        }
+
+        var cursorToPoint = randomPoint()
+        while (true) {
+            tween(
+                cursorBG::x[cursorToPoint.x],
+                cursorBG::y[cursorToPoint.y],
+                easing = Easing.EASE_IN_OUT
+            )
+            cursorToPoint = randomPoint()
         }
     }
 }
+
+fun randomPoint() =
+    Point(Random.nextInt(0..screenWidth), Random.nextInt(0..screenHeight))
--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -9,10 +9,10 @@ val screenHeight = 512
 suspend fun main() = Korge(Korge.Config(module = ConfigModule))
 
 object ConfigModule : Module() {
-	override val size: SizeInt = SizeInt(screenWidth, screenHeight)
+    override val size: SizeInt = SizeInt(screenWidth, screenHeight)
 
-	override val mainScene = MainMenuScene::class
-	override suspend fun AsyncInjector.configure() {
-		mapPrototype { MainMenuScene() }
-	}
+    override val mainScene = MainMenuScene::class
+    override suspend fun AsyncInjector.configure() {
+        mapPrototype { MainMenuScene() }
+    }
 }
